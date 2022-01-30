25c45d62de183a226497b3e21f87fd3c6fc57b1d
Likita <likitarai1@gmail.com>
2022-01-30 16:53:18 +0530

    intuit13

--- Intuit/I13.java
+++ Intuit/I13.java
@@ -1,51 +1,41 @@
-public int minSwaps(int[][] grid) {
-        int[] a = gridToVec(grid);
-        return minSwaps(a);
-    }
-    
-    private int minSwaps(int[] a) {
-        int n = a.length;
-        int ans = 0;
-        for (int i=0; i<n; i++) {
-            if (a[i] < (n-i-1)) {
-                int j=i;
-                while (j < n && a[j] < (n-i-1)) {
-                    j++;
+class Solution {
+    public int minSwaps(int[][] grid) {
+        ArrayList<Integer> al = new ArrayList<>();
+        int n = grid.length;
+        int swaps = 0;
+        
+        for(int i=0;i<n;i++){
+            int zerosAtEnd = 0;
+            for(int j=n-1;j>=0;j--){
+                if(grid[i][j]==1){
+                    break;
                 }
-				
-                if (j == n) {     // Did not find any number greater than or equal to n-i-1.
-                    return -1;  // so its impossible to get the answer.
+                else{
+                    zerosAtEnd++;
                 }
-                while (j>i) {
-                    //swap(a[j], a[j-1]);
-                    int temp = a[j];
-                    a[j] = a[j-1];
-                    a[j-1] = temp;
-                    ans++;
-                    j--;
+            }
+            al.add(zerosAtEnd);
+        }
+        
+        for(int i=0;i<n;i++){
+            int zerosNeeded = n-i-1;
+            for(int j=i;j<al.size();j++){
+                if(al.get(j)>=zerosNeeded){
+                    swaps += j - i;
+                    al.add(0, al.get(j));
+                    al.remove(j+1);
+                    break;
+                }
+                
+                if(j==n-1){
+                    return -1;
                 }
             }
         }
-        return ans;
+        
+        return swaps;
     }
-    
-    
-    private int zerosAtEnd(int[] a) {
-        int ans = 0;
-        int n = a.length;
-        int i=n-1;
-        while (i >= 0 && a[i] == 0) {
-            ans++;
-            i--;
-        } 
-        return ans;
-    }
-    
-     private int[] gridToVec(int[][] grid) {
-        int[] ans = new int[grid.length];
-         int i =0;
-        for (int[] x: grid) {
-            ans[i++] = zerosAtEnd(x);
-        }
-        return ans;
-    }
+}
+
+// TC = O(N^2)
+// SC = O(N)
