6a60223768c94a0a34ca8898a67697f209cd056a
Likita <likitarai1@gmail.com>
2022-02-01 12:55:37 +0530

    I2

--- Intuit/I2.java
+++ Intuit/I2.java
@@ -1,56 +1,44 @@
-
-import java.io.*;
-import java.util.*;
 class Solution {
-
-	static int R, C;
-
-	static int[] x = { -1, -1, -1, 0, 0, 1, 1, 1 };
-	static int[] y = { -1, 0, 1, -1, 1, -1, 0, 1 };
-
-	static boolean search2D(char[][] grid, int row,
-							int col, String word)
-	{
-		if (grid[row][col] != word.charAt(0))
-			return false;
-
-		int len = word.length();
-
-		for (int dir = 0; dir < 8; dir++) {
-		
-			int k, rd = row + x[dir], cd = col + y[dir];
-
-			for (k = 1; k < len; k++) {
-		
-				if (rd >= R || rd < 0 || cd >= C || cd < 0)
-					break;
-
-				if (grid[rd][cd] != word.charAt(k))
-					break;
-
-				rd += x[dir];
-				cd += y[dir];
-			}
-
-			if (k == len)
-				return true;
-		}
-		return false;
-	}
-
-	static void patternSearch(
-		char[][] grid,
-		String word)
-	{
-		for (int row = 0; row < R; row++) {
-			for (int col = 0; col < C; col++) {
-				if (search2D(grid, row, col, word))
-					System.out.println(
-						"pattern found at " + row + ", " + col);
-			}
-		}
-	}
-
-
+    int m, n, len;
+    public boolean dfs(int r, int c, int start, String word, char[][] board){
+        
+        if(r>=m || r<0 || c<0 || c>=n){
+            return false;
+        }
+        
+        if(start==len-1 && board[r][c]==word.charAt(start)){
+            return true;
+        }
+        
+        if(word.charAt(start)!=board[r][c]){
+            return false;
+        }
+        
+        boolean ans = false;
+        board[r][c] = '#';
+        ans = dfs(r-1,c,start+1,word,board) || dfs(r,c+1,start+1,word,board) || dfs(r+1,c,start+1,word,board) || dfs(r,c-1,start+1,word,board);
+        board[r][c] = word.charAt(start); //backtrack
+        return ans;
+    }
+    
+    public boolean exist(char[][] board, String word) {
+        m = board.length;
+        n = board[0].length;
+        
+        len = word.length();
+        int start = 0;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(dfs(i, j, start, word, board)){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
 }
 
+// TC = O(N*3^L)
+// SC = O(L)
